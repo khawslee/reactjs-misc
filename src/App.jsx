35bf4b62382951f454d6d45836f8deac69c7c7c0
Siang Lee, Khaw <khawslee@gmail.com>
2023-06-04 15:56:48 +0800

    Tictactoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 //import Todolist from "./projects/01-todolist/Todolist";
-import Ecommerce from "./projects/02-ecommerce/Ecommerce";
+// import Ecommerce from "./projects/02-ecommerce/Ecommerce";
+import Board from "./projects/03-tictactoe/Tictactoe"
 
 export default function App() {
   return (
-    <Ecommerce />
+    // <Ecommerce />
+    <Board />
   )
 }
